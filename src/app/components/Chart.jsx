6978c5d3e7c03c548c5bdb1140fa7508eb63055d
Mohamed Hagi <mohamed.2423@hotmail.com>
2023-05-05 15:50:12 -0400

    changes

--- src/app/components/Chart.jsx
+++ src/app/components/Chart.jsx
@@ -3,8 +3,6 @@ import { Chart as ChartJS, LineElement, CategoryScale, LinearScale, PointElement
 import useData from '../DataContext';
 ChartJS.register(LineElement, CategoryScale, LinearScale, PointElement);
 export default function Chart() {
-	const { data } = useData();
-	console.log(data);
 	return (
 		<div>
 			<h1>Chart</h1>

4eea3ac7980dc45247a752b5aac2506db39062a3
Mohamed Hagi <mohamed.2423@hotmail.com>
2023-05-05 15:58:19 -0400

    changes

--- src/app/DataContext.js
+++ src/app/DataContext.js
@@ -1,41 +1,38 @@
-import { createContext, useReducer, useContext } from 'react'
-import dataReducer, { intialState } from "./DataReducer"
+import { createContext, useReducer, useContext } from 'react';
+import dataReducer, { intialState } from './dataReducer';
 import { csv } from 'd3';
 
 const DataContext = createContext(intialState);
 
 export const DataProvider = ({ children }) => {
-const[ state, dispatch ] = useReducer(dataReducer, intialState);
+	const [state, dispatch] = useReducer(dataReducer, intialState);
 
-const filterByYear = (year) => {
-    csv("/data.csv").then((data) => {
-        const filteredData = data.filter((currentData) => currentData["Year"] === year)
-        dispatch({
-            type: "FILTER_BY_YEAR",
-            payload: {
-                data: filteredData
-            }
-        })
-      });
-     
-}
+	const filterByYear = (year) => {
+		csv('/data.csv').then((data) => {
+			const filteredData = data.filter((currentData) => currentData['Year'] === year);
+			dispatch({
+				type: 'FILTER_BY_YEAR',
+				payload: {
+					data: filteredData,
+				},
+			});
+		});
+	};
 
-const value = {
-    data: state.data,
-    filterByYear
-
-}
-return <DataContext.Provider value={value}>{children}</DataContext.Provider>
-
-}
+	const value = {
+		data: state.data,
+		filterByYear,
+	};
+	return <DataContext.Provider value={value}>{children}</DataContext.Provider>;
+};
 
 const useData = () => {
-    const context = useContext(DataContext)
-    if (context === undefined){
-        throw new Error("useData must be used within DataContext")
-    }
+	const context = useContext(DataContext);
+	if (context === undefined) {
+		throw new Error('useData must be used within DataContext');
+	}
 
-    return context
-}
+	return context;
+};
 
-export default useData
+export default useData;
